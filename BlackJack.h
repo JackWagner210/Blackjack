9816141226aa001a3ab6f48c7449e065b858c2d7
JackWagner210 <jrw210@txstate.edu>
2017-12-03 16:12:52 -0600

    Initial commit

---
+++ BlackJack.h
@@ -0,0 +1,10 @@
+#ifndef BLACKJACK_H_INCLUDED
+#define BLACKJACK_H_INCLUDED
+class Card{
+public:
+    Card();
+
+};
+
+
+#endif // BLACKJACK_H_INCLUDED
---
+++ Blackjack.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+const int DECK_SIZE = 52;
+
+struct card{
+    int value;
+    String suit;
+};
+
+int main()
+{
+    int playerCash;
+    int dealerCash;
+
+    return 0;
+}
